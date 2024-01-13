6d4e493688135cdd4bd01ccf2b4db97755888a4c
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-01-13 23:30:16 +0530

    adding required files

--- temp.c
+++ temp.c
@@ -1,3 +1,9 @@
+/*
+This is a temporary file created in C language just for the purpose of trying out new things.
+I mean like, a temporary file where smaller snippets to a larger meaningful code could be tried out here.
+That code snippet could be analyzed for bugs here and then can be later integrated meaningfully to the larger parent code block.
+*/
+
 #include <stdio.h>
 
 int main()
