9e0d58151bf04ff2b047e72f1b358583e08a3567
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-01-13 21:43:47 +0530

    added required files

--- apna-college-c-tutorial/divisible by 2 or not.c
+++ apna-college-c-tutorial/divisible by 2 or not.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+// This same code can be implemented for checking is a given number is odd or even
+
 int main ()
 {
     int num ;
+    // 1 if the number is divisible by 2
+    // 0 if the number is not divisible by 2
     printf("Enter a number - ");
     scanf("%d", &num );
     printf("%d", num%2 == 0);
